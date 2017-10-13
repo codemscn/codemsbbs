b612aa0b10e281957aeef20d96f6f7d0e1758783
vincenth520 <1091986039@qq.com>
2017-10-13 18:27:18 +0800

    add login regist

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,10 @@
 |
 */
 
-Route::get('/', 'IndexController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
